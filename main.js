d6267f8d02ddf19ca59b8f289ee9aeab09a9dbf4
faizaanvidhani <faizaanvidhani@gmail.com>
2019-02-16 14:15:52 -0600

    Initialize file structure

---
+++ main.js
@@ -0,0 +1,5 @@
+var style = require('./node_modules/bootstrap/dist/css/bootstrap.css')
+var $ = require('jquery');
+var bootstrap = require('bootstrap/dist/js/bootstrap');
+
+alert('Welcome to Hebron TSA');
